333e4f694953b61d3909dd85e7dc590853e07f8a
Florian Krauß <Floh@lore.fritz.box>
2010-01-23 14:21:14 +0100

    zeichnet zu jedem ziel eine  linie

--- src/socket/ConnectionClient.java
+++ src/socket/ConnectionClient.java
@@ -14,14 +14,14 @@ public class ConnectionClient {
 		//String zuSendendeNachricht = "<map><name>ich</name><pos>"+(8+i)+","+((i*2)+2)+"</pos><direction>3,6</direction><data>1,"+i+"</data>" +
 
 		//"<data>10,5</data><data>23,4</data><data>"+i+",3</data></map>";
-		//for(int x=0;x<7;x++)
-		//{
-			for(int y=0;y<7;y++)
+		for(int x=0;x<20;x++)
+		{
+			for(int y=0;y<20;y++)
 			{
 				Socket socket = new Socket(ip, port); // verbindet sich mit Server
 		
 				//	String zuSendendeNachricht = "<map><name>ichj</name><pos>"+(i+15)+",1</pos><direction>"+(i+1)+",0</direction><data>10,10</data></map>"; // direction sind cm daraus ergibt sich der alpha
-				String zuSendendeNachricht = "<map><name>ichj</name><pos>0,0</pos><direction>0,0</direction><data>10,10</data><data>11,10</data><data>12,10</data><data>13,10</data><data>14,10</data><data>15,10</data></map>"; // direction sind cm daraus ergibt sich der alpha
+				String zuSendendeNachricht = "<map><name>ichj</name><pos>"+x+","+y+"</pos><direction>0,0</direction><data>10,10</data><data>11,10</data><data>12,10</data><data>13,10</data><data>14,10</data><data>15,10</data><data>15,15</data></map>"; // direction sind cm daraus ergibt sich der alpha
 			
 				int len = zuSendendeNachricht.length();
 				zuSendendeNachricht = len + zuSendendeNachricht;
@@ -30,8 +30,8 @@ public class ConnectionClient {
 				y++;
 				//Thread.sleep(1000);
 			}
-		//x++;
-		//}
+		x++;
+		}
 	}
 
 	public void schreibeNachricht(Socket socket, String nachricht) throws IOException {
